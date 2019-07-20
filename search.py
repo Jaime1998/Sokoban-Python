98aa1c52ae6e06fa33106b40d2fa77a248c4a45b
Jaime1998 <jaime.cuartas@correounivalle.edu.co>
2019-07-20 09:09:38 -0500

    Identificación de puntos muertos para podar nodos que expandan subarboles que no llevaran a la solución por encontrarse las cajas en lugares como esquinas

--- search.py
+++ search.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from state import *
 
+if __name__ == '__main__':
+    obstacles = [(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (1,0), (1,4), (1,5), (2,0), (2,5), (3,0), (3,5), (4,0), (4,5), (5,0), (5,1), (5,2), (5,3), (5,4), (5,5)]
+    storages = {(2,1): 0, (2,3):1}
+    stateObj = State((1,2), {(2,2):0, (2,3):1})
+    stateObj.possibleMoves(storages, obstacles)
+    
--- state.py
+++ state.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-
-
 class State():
     
     #player coordintate (#,#)
@@ -42,24 +39,108 @@ class State():
                 newBoxesPos[newBoxPos] = i
             
             newState = State(newPlayerPos, newBoxesPos)
+            
             print ('Las cajas están en: ')
             print (newState.boxes)
             print ('El jugador está en: ')
             print (newState.player)
             possibleMoves.append(newState)
+        return possibleMoves
+
+    #return True if is a deadlock, False in otherwise
+    def isDeadLock(storagesIn, obstaclesIn):
+        for coordinateBox in self.box:
+            if coordinateBox in storagesIn:
+                continue
+            else:
+                
+                l = tuple(map(int.__add__, coordinateBox, (0,-1)))
+                r = tuple(map(int.__add__, coordinateBox, (0,1)))
+                up = tuple(map(int.__add__, coordinateBox, (-1,0)))
+                bot = tuple(map(int.__add__, coordinateBox, (1,0)))
+
+                #diagonals
+                #up and right
+                dur = tuple(map(int.__add__, coordinateBox, (-1,1)))
+                #up and left
+                dul = tuple(map(int.__add__, coordinateBox, (-1,-1)))
+                #bot and right
+                dbr = tuple(map(int.__add__, coordinateBox, (1,1)))
+                #bot and left
+                dbl = tuple(map(int.__add__, coordinateBox, (1,-1)))
+
+                #################################
+                #            box  w
+                #             w  any
+                if (r in obstaclesIn) and (bot in obstaclesIn):
+                    return True
+
+                #################################
+                #             w  box
+                #            any  w
+                if (l in obstaclesIn) and (bot in obstaclesIn):
+                    return True
+                
+                #################################
+                #            any  w
+                #             w  box
+                if (l in obstaclesIn) and (up in obstaclesIn):
+                    return True
+                
+                #################################
+                #             w  any
+                #            box  w
+                if (r in obstaclesIn) and (up in obstaclesIn:):
+                    return True
                 
 
-def goalState(stateIn, storages):
-    for box in stateIn.boxes:
-        if box not in storages:
-            return False
-    return True
+                #################################
+                # Type of deadblock, right place
+                #            box  box/w
+                #           box/w box/w
+                if (r in obstaclesIn) or (r in self.boxes):
+                    if (bot in obstaclesIn) or (bot in self.boxes):
+                        if (dbr in obstaclesIn) or (dbr in self.boxes):
+                            return True
+                    if (up in obstaclesIn) or (up in self.boxes):
+                        if (dur in obstaclesIn) or (dur in self.boxes):
+                            return True
+                #################################
+                # Type of deadblock, left place
+                #           box/w  box
+                #           box/w box/w
+                if (l in obstaclesIn) or (l in self.boxes):
+                    if (bot in obstaclesIn) or (bot in self.boxes):
+                        if (dbl in obstaclesIn) or (dbl in self.boxes):
+                            return True
+                    if (up in obstaclesIn) or (up in self.boxes):
+                        if (dul in obstaclesIn) or (dul in self.boxes):
+                            return True
+        rerturn False        
+
+    def isGoalState(storagesIn):
+        for box in self.boxes:
+            if box not in storagesIn:
+                return False
+        return True
+
+class Node():
+
+    def __init__(self, player, boxes, movement, parent):
+        self.stateNode = State(player, boxes)
+        self.movement = movement
+        self.parent = parent
+    
+    
 
 
+
+
+"""
 obstacles = [(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (1,0), (1,4), (1,5), (2,0), (2,5), (3,0), (3,5), (4,0), (4,5), (5,0), (5,1), (5,2), (5,3), (5,4), (5,5)]
 storages = {(2,1): 0, (2,3):1}
 stateObj = State((1,2), {(2,2):0, (2,3):1})
-stateObj.possibleMoves(storages, obstacles)
+stateObj.possibleMoves(storages, obstacles)"""
     
 
 
